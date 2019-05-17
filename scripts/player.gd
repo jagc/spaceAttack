523bee1326149fee1f503f6eced221e53ab285c2
jagc <jag.cordoviz@gmail.com>
2019-05-17 22:14:05 +0800

    bounded player to screen borders

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,13 @@ extends KinematicBody2D
 const SPEED = 500
 
 var screen_size
+var half_sprite_size
+
+onready var sprite = $Sprite
 
 func _ready():
 	screen_size = get_viewport_rect().size.x
+	half_sprite_size = sprite.texture.get_width() * scale.x / 2
 	
 func _process(delta):
 	if Input.is_action_pressed("left"):
@@ -15,4 +19,4 @@ func _process(delta):
 	elif Input.is_action_just_pressed("close"):
 		get_tree().quit()
     
-	position.x = clamp(position.x, 0, screen_size)
+	position.x = clamp(position.x, 0 + half_sprite_size, screen_size - half_sprite_size)
