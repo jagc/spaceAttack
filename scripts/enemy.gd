416f28bf993415d40dc083cff66e5884b6b56a3a
jagc <jag.cordoviz@gmail.com>
2019-05-18 03:29:28 +0800

    added enemy projectile, enemy is shooting upon spawn

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export(PackedScene) var projectile
+
 onready var timer = $Timer
 onready var collision = $CollisionShape2D
 
@@ -7,6 +9,16 @@ export var speed = 50
 export var health = 30
 
 var dead = false
+var can_shoot = true
+
+func _process(delta):
+	if can_shoot:
+		_shoot()
+
+func _shoot():
+	var new_projectile = projectile.instance()
+	new_projectile.position = global_position
+	get_tree().get_root().add_child(new_projectile)
 
 func add_damage(damage):
 	health -= damage
