423cd45db9f4d71e3c2fedf99c64ce25faf4d691
jagc <jag.cordoviz@gmail.com>
2019-05-17 21:57:04 +0800

    clamped player to screen width. Added close/exit functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,17 @@ extends KinematicBody2D
 
 const SPEED = 500
 
+var screen_size
+
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size.x
 	
 func _process(delta):
 	if Input.is_action_pressed("left"):
 		position.x -= SPEED * delta
 	elif Input.is_action_pressed("right"):
 		position.x += SPEED * delta
+	elif Input.is_action_just_pressed("close"):
+		get_tree().quit()
     
+	position.x = clamp(position.x, 0, screen_size)
